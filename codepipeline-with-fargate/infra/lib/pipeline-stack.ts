0354f70c448474383d799be4e5fd5cadc85f1bd1
Niko <nrsandholm@gmail.com>
2023-01-21 08:57:54 +0200

    Fix 'no matching base directory path found for cdk.out'

--- codepipeline-with-fargate/infra/lib/pipeline-stack.ts
+++ codepipeline-with-fargate/infra/lib/pipeline-stack.ts
@@ -27,7 +27,9 @@ export class PipelineStack extends Stack {
           'cd codepipeline-with-fargate/infra',
           'npm install',
           'npm run cdk synth',
-        ]
+        ],
+        // Define this if you're not working at the root of the repo
+        primaryOutputDirectory: 'codepipeline-with-fargate/infra/cdk.out',
       })
     });
 
